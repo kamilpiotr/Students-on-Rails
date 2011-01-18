3d9a2d4fe81ac985a85e27322ecc918b1de50d59
BartekK <kossakowski.b@gmail.com>
2011-01-18 19:10:09 +0100

    Stworzenie struktury Aplikacji, edycja pliku .rvmrc oraz GEMFILE oraz wrzucenie potrzebnych plików css,js i obrazków

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ap::Application.config.secret_token = 'dae03b957786a981ffe350f1e2e5a11481a6553ad2cba12908ffa3160346aba52457bf08d0af19ca80e1a808e785d7028e191e4f7155664a1aacc3a69bd6fefc'
