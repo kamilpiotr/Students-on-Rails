275f6f4b0cfa056aee2e6bb0b45e32dc6bfc639e
kamilpiotr <kurowskikamil@yahoo.com>
2011-01-18 18:33:56 +0100

    pusto

--- app/controllers/comments_controller.rb
+++
@@ -1,7 +0,0 @@
-class CommentsController < ApplicationController
-	def create
-		@place = Place.find(params[:place_id])
-		@comment = @place.comments.create!(params[:comment])
-		respond_with(@place)
-	end
-end
