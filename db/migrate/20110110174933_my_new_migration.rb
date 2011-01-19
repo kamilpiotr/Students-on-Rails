5c8baeb238959815bd2fca4ff86ecd7064dd8a7c
kamilpiotr <kurowskikamil@yahoo.com>
2011-01-19 14:06:50 +0100

    porzadek 2

--- db/migrate/20110110174933_my_new_migration.rb
+++
@@ -1,9 +0,0 @@
-class MyNewMigration < ActiveRecord::Migration
-   def self.up
-    remove_column :comments, :post_id
-    add_column :comments, :place_id, :integer
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110110185844_m1.rb
+++
@@ -1,9 +0,0 @@
-class M1 < ActiveRecord::Migration
-  def self.up
-  drop_table :places
-  drop_table :comments
-  end
-
-  def self.down
-  end
-end
